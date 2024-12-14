86dcb34b6c824173bb6227202a157f3e4b9f0144
Christain Jeric Non <cjn2023-2078-86841@bicol-u.edu.ph>
2024-12-15 00:20:18 +0800

    delete failed project

--- GYMFIT-INTERFACE2/sign-in_sign-up/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'connection.php'; // Ensure config.php is included to start the session
-
-session_destroy();  // Destroy all sessions
-header("Location: login.php");  // Redirect to login
-exit();  // Ensure no further code is executed
-?>
